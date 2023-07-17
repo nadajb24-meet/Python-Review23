41247dbf58f5956bc2f7d495bc493eeb8690f31b
nadajb24-meet <nada.jabarin@gmail.com>
2023-07-17 12:09:43 +0300

    finished

--- review.py
+++ review.py
@@ -1,16 +1,26 @@
 def create_youtube_video(title,description):
-	youtube_video = {"title" : title,"description" : description,"likes" :0,"dislikes":0,'comments':{}}
+	youtube_video = {"title" : title,"description" : description,"likes" :0,"dislikes":0,'comments':{'username':userame}}
 	return youtube_video
 
 def like(video):
 	if likes in video:
-		likes+=1
+		video['likes']+=1
 
 def dislike(video):
 	if dislikes in video:
-		dislikes+=1
+		video['dislikes']+=1
 
 
 def add_comment(youtubevideo,username,comment_text):
 	video["comment"][username]=comment_text
 	return video
+
+nada=create_youtube_video('beach', 'love love love')
+print(nada)
+like(nada)
+dislike(nada)
+add_comment(nada,'nada','we love the beach')
+print(nada)
+for i in range(495):
+	like(nada)
+print(nada)
